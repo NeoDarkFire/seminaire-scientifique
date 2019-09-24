66f48ed4baf5b709b7884b646b5461443673b899
Aklinar <Aklinar@10.54.128.151>
2019-09-24 09:27:26 +0200

    github ça pue

---
+++ src/main/java/view/ViewFacade.java
@@ -0,0 +1,29 @@
+package view;
+
+import mvc.IController;
+import mvc.IModel;
+import mvc.ISignal;
+import mvc.IView;
+
+public class ViewFacade implements IView
+{
+
+	@Override
+	public void attachController(IController controller)
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void notifyControllers(ISignal signal)
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onModelEvent(IModel model, Object arg)
+	{
+		// TODO Auto-generated method stub
+	}
+	
+}

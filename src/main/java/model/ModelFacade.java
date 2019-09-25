f8277a19aa488e287771d5445a9f259a25c4bb15
NeoDarkFire <dark.fire@gmx.fr>
2019-09-25 13:01:36 +0200

    Added controller and model skeleton

---
+++ src/main/java/model/ModelFacade.java
@@ -0,0 +1,17 @@
+package model;
+
+import mvc.IModel;
+import mvc.ISignal;
+import mvc.IView;
+
+public class ModelFacade implements IModel {
+    @Override
+    public void attachView(final IView view) {
+        // TODO: method stub
+    }
+
+    @Override
+    public void notifyViews(final ISignal signal) {
+        // TODO: method stub
+    }
+}

92bb9b6e1bb8e890cfd0c6ab43e3bcef2f07ba2a
BrunEmilien <38348712+BrunEmilien@users.noreply.github.com>
2019-09-25 15:12:36 +0200

    ModelFacde et ce qui va avec.

--- src/main/java/model/Map_P.java
+++ src/main/java/model/Map_P.java
@@ -7,29 +7,48 @@ import java.util.ArrayList;
 
 public abstract class Map_P extends AbstractDAO{
 
-	final static private String selectIDbyLoginsInformations = "SELECT ID FROM utilisateurs WHERE login = ? AND mdp = ?;";
+	final static private String selectIDbyLogin = "SELECT ID FROM utilisateurs WHERE login = ?;";
+	final static private String selectIDbyLoginsAndPassword = "SELECT ID FROM utilisateurs WHERE login = ? AND mdp = ?;";
 
 	public static int selectIDbyloginPassword(String login, String password) {
-		//return 0 - Error
-		//return 1 - login fail
 		//return 2 - mdp fail
 		//return 3 - success
-		final CallableStatement callStatement = prepareCall(selectIDbyLoginsInformations);
+		final CallableStatement callStatement = prepareCall(selectIDbyLogin);
 		ArrayList<Integer> response = new ArrayList<Integer>();
         try {
 			callStatement.setString(1, login);
-			callStatement.setString(2, password);
 	        if (callStatement.execute()) {
 	            final ResultSet result = callStatement.getResultSet();
 	            while(result.next()) {
 	            	response.add( result.getInt(1)); //TODO A CHANGER password / login sql
 	            }
+	            if (response.size() > 0) {
+	            	final CallableStatement callStatements = prepareCall(selectIDbyLoginsAndPassword);
+	        		ArrayList<Integer> responses = new ArrayList<Integer>();
+	                try {
+	        			callStatements.setString(1, login);
+	        			callStatements.setString(2, password);
+	        	        if (callStatements.execute()) {
+	        	            final ResultSet results = callStatements.getResultSet();
+	        	            while(results.next()) {
+	        	            	responses.add( results.getInt(1)); //TODO A CHANGER password / login sql
+	        	            }
+	        	            if (responses.size() == 1) {
+	        	            	return 3; //success
+	        	            } else {
+	        	            	return 2; //mdp fail
+	        	            }
+	        	        }
+	                } catch (SQLException e) {
+	                	
+	                }
+	            } else {
+	            	return 1; //login fail
+	            }
 	            result.close();
 	        }
         } catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-		return 0; //TODO a changer
+		return 0; // Error
 	}
 }
--- src/main/java/model/ModelFacade.java
+++ src/main/java/model/ModelFacade.java
@@ -1,29 +1,54 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
 
 import mvc.IModel;
 import mvc.ISignal;
 import mvc.IView;
+import signal.LoginSignal;
+import signal.LoginSignal.Status;
 
-public class ModelFacade implements IModel {
+public class ModelFacade extends Observable implements IModel {
+	
 	
 	public void decrypt(String in, String out) {
-		//TODO a refaire
+		//TODO Alexis
 	}
 	
 	public void selectIDbyloginPassword(String login, String password) {
 		int result = Map_P.selectIDbyloginPassword(login, password);
-		//TODO renvoyer un signal
+		switch(result){
+			case 0:
+				//TODO error
+				break;
+			case 1:
+				notifyViews(new LoginSignal(Status.LOGIN_FAILED));
+				break;
+			case 2:
+				notifyViews(new LoginSignal(Status.PASSWORD_FAILED));
+				break;
+			case 3:
+				notifyViews(new LoginSignal(Status.SUCCESS));
+				break;
+			default:
+				//TODO error
+				break;
+		}
 	}
 	
-    @Override
-    public void attachView(final IView view) {
-        // TODO: method stub
+	@Override
+    public void attachView(IView view)
+    {
+        this.addObserver((Observer) view);
+        view.onModelEvent(this, null);
     }
 
     @Override
-    public void notifyViews(final ISignal signal) {
-        // TODO: method stub
+    public void notifyViews(ISignal signal)
+    {
+        this.setChanged();
+        this.notifyObservers(signal);
     }
 }
